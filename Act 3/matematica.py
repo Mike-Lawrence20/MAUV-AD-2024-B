51d4349fdc77a7c43d46c4ae2383c22d139e4f36
Mike-Lawrence20 <miguel.auv@hotmail.com>
2024-08-19 23:30:13 -0600

    Add files via upload

---
+++ Act 3/matematica.py
@@ -0,0 +1,11 @@
+def suma(a, b):
+    """
+    Retorna la suma de dos números.
+    """
+    return a + b
+
+def resta(a, b):
+    """
+    Retorna la resta de dos números.
+    """
+    return a - b
